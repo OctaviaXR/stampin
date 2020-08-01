4f5f10d13fffffad9a8596226e1cd22f4f5c8ff9
Zack Lee <cuinjune@gmail.com>
2020-08-01 11:00:10 -0400

    add airplane gltf model

--- js/scene.js
+++ js/scene.js
@@ -18,22 +18,25 @@ class Scene {
 
     init() {
         // camera
-        this.initCameraPosition = new THREE.Vector3(0, 0, 5);
-        this.camera.position.set(this.initCameraPosition);
+        this.initCameraPosition = new THREE.Vector3(-3.2, 1.1, 0.6);
+        this.camera.position.set(this.initCameraPosition.x, this.initCameraPosition.y, this.initCameraPosition.z);
 
         // lighting
-        this.light = new THREE.PointLight(0xFFFFFF, 1);
-        this.light.position.set(0, 0, 5);
+        this.light = new THREE.PointLight(0xFFFFFF, 2);
+        this.light.position.set(this.initCameraPosition.x, this.initCameraPosition.y + 1, this.initCameraPosition.z);
         this.scene.add(this.light);
 
-        // room mesh
-        const geometry = new THREE.SphereBufferGeometry(500, 60, 40);
-        // invert the geometry on the x-axis so that all of the faces point inward
-        geometry.scale(-0.1, 0.1, 0.1);
-        const texture = new THREE.TextureLoader().load('https://threejs.org/examples/textures/2294472375_24a3b8ef46_o.jpg');
-        const material = new THREE.MeshBasicMaterial({ map: texture });
-        const mesh = new THREE.Mesh(geometry, material);
-        this.scene.add(mesh);
+        // model
+        const loader = new THREE.GLTFLoader();
+        const url = "assets/models/airbus_a320_airplane_cabin/scene.gltf";
+        loader.load(url, (gltf) => {
+            const box = new THREE.Box3().setFromObject(gltf.scene);
+            const center = box.getCenter(new THREE.Vector3());
+            gltf.scene.position.x += (gltf.scene.position.x - center.x);
+            gltf.scene.position.y += (gltf.scene.position.y - center.y);
+            gltf.scene.position.z += (gltf.scene.position.z - center.z);
+            this.scene.add(gltf.scene);
+        });
 
         // events
         window.addEventListener("resize", () => {
@@ -53,9 +56,9 @@ class Scene {
         Decode(this.yaw, this.pitch, this.roll);
 
         // change camera position
-        const cameraPostionXRange = 50;
-        const cameraPostionYRange = 50;
-        const cameraPostionZRange = 50;
+        const cameraPostionXRange = 0.7;
+        const cameraPostionYRange = 0.7;
+        const cameraPostionZRange = 0.7;
         const cameraPostionX = this.initCameraPosition.x + window.nomalizedPosition.x * cameraPostionXRange - (cameraPostionXRange / 2);
         const cameraPostionY = this.initCameraPosition.y + (1 - window.nomalizedPosition.y) * cameraPostionYRange - (cameraPostionYRange / 2);
         const cameraPostionZ = this.initCameraPosition.z + (1 - window.nomalizedPosition.z) * cameraPostionZRange - (cameraPostionZRange / 2);
@@ -63,7 +66,7 @@ class Scene {
 
         // change camera rotation
         const cameraRotationXDeg = this.pitch * 0.5;
-        const cameraRotationYDeg = -this.yaw * 0.25;
+        const cameraRotationYDeg = -this.yaw * 0.5;
         const cameraRotationZDeg = this.roll * 0.5;
         this.camera.rotation.set(THREE.Math.degToRad(cameraRotationXDeg), THREE.Math.degToRad(cameraRotationYDeg), THREE.Math.degToRad(cameraRotationZDeg));
 
