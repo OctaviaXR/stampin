95c7f6c3d13c1df4ac81ea5805bea0476bf780b8
Zack Lee <cuinjune@gmail.com>
2020-08-02 09:05:52 -0400

    setup nodejs

---
+++ index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// set your static server
+const publicPath = path.resolve(`${__dirname}/public`);
+app.use(express.static(publicPath));
+
+// views
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+// start listening
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+})
